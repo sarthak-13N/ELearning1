5159bd16175346cd831d704b4c9c606b55c4e55c
sumedh <sumedhkedari6@gmail.com>
2024-08-04 11:38:47 +0530

    added a updates on home and profile my sumedh on 4 augt

--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,TemplateRef, ViewChild  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router,RouterLink, RouterModule } from '@angular/router';
 import { CourseService, Course } from './course.service';
-
+import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-course-list',
   standalone: true,
@@ -11,13 +11,34 @@ import { CourseService, Course } from './course.service';
   styleUrls: ['./course-list.component.css']
 })
 export class CourseListComponent implements OnInit {
-  courses: Course[] = [];
+  @ViewChild('courseInfoTemplate') courseInfoTemplate!: TemplateRef<any>;
+  
+  // constructor(private dialog: MatDialog) {}
 
-  constructor(private courseService: CourseService) { }
+  courses: any[] = [
+    {
+      name: 'JavaScript',
+      imageUrl: 'data:image/png;base64,/9j/',
+      description: 'Learn JavaScript from scratch.',
+      price: 19.99,
+      modules: [
+        { title: 'Introduction', description: 'Getting started with JavaScript' },
+        { title: 'Functions', description: 'Understanding functions' }
+      ]
+    },
+    // Add more courses here
+  ];
+  
+  selectedCourse: any;
+  constructor(private courseService: CourseService,private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.courseService.getCourses().subscribe(courses => {
       this.courses = courses;
     });
+  }
+  openCourseInfo(course: any): void {
+    this.selectedCourse = course;
+    this.dialog.open(this.courseInfoTemplate);
   }
 }
