e16a20860c0f69bbbd339d751735299c0aacfb8f
sumedh kedari <sumedhkedari6@example.com>
2024-08-05 10:49:35 +0530

    5/8/2024 sarthak

---
+++ src/app/timepass/timepass.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timepass',
+  standalone: true,
+  imports: [],
+  templateUrl: './timepass.component.html',
+  styleUrl: './timepass.component.css'
+})
+export class TimepassComponent {
+
+}
